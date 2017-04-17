a2e36a8d3c1470208048631a9c0da9b77195d82b
stormhu <stormhu@foxmail.com>
2017-04-17 22:05:54 +0800

    小修改

--- admin/config.php
+++ admin/config.php
@@ -143,7 +143,11 @@ return [
     // +---------------------------------------------------------------------
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+        // admin css位置
+        '__CSS__'       => '/public/static/admin/css/',
+        '__JS__'        => '/public/static/admin/js/',
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
